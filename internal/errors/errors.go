5b9287b95ff0335927c4849a08ac2a57d5189822
qingshangithub <1056914402@qq.com>
2022-03-18 16:24:05 +0800

    fix: 自定义Error的边界修订

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -24,6 +24,7 @@ func (e *HTTPError) Error() string {
 	return fmt.Sprintf("HTTPError: %d", e.Code)
 }
 
+// FromError try to convert an error to *HTTPError.
 func FromError(err error) *HTTPError {
 	if err == nil {
 		return nil
